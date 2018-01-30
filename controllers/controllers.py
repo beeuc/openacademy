776c79e518f91ec4128c1f4d80a36176effbfece
zhongmou <zhongmou@mulu.online>
2018-01-30 12:15:44 +0800

    made some progress

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class Openacademy(http.Controller):
-#     @http.route('/openacademy/openacademy/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Openacademy(http.Controller):
+    @http.route('/openacademy/openacademy/', auth='public', website=True)
+    def index(self, **kw):
+        Teachers = http.request.env['openacademy.teachers']
+        return http.request.render('openacademy.index', {
+            'teachers': Teachers.search([])
+        })
 
+    @http.route('/openacademy/<model("openacademy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('openacademy.biography', {
+            'person': teacher
+        })
 #     @http.route('/openacademy/openacademy/objects/', auth='public')
 #     def list(self, **kw):
 #         return http.request.render('openacademy.listing', {
--- models/models.py
+++ models/models.py
@@ -2,10 +2,10 @@
 
 from odoo import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class openacademy(models.Model):
+     _name = 'openacademy.teachers'
 
-#     name = fields.Char()
+     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
